635e673781eb51d673ec01d26cec3f53a2084b78
hee1x <whaihoek@gmail.com>
2024-04-10 11:30:17 +0800

    added sass

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = 3000
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/views/public'));
+app.use('/styles', express.static(__dirname + '/views/public/styles'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 app.get('/', function(req, res) {
