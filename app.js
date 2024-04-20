0f0ff5e91b4a8c0d7b2682ed564ebb7984a19426
hee1x <kwokwhaihoe@gmail.com>
2024-04-20 21:03:48 +0800

    added new diners club project page WIP

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = 3000
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/views/public'));
+app.use('/partials', express.static(__dirname + '/views/public/partials'));
 app.use('/styles', express.static(__dirname + '/views/public/styles'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
@@ -15,6 +16,14 @@ app.get('/', function(req, res) {
 
 app.get('/contact-me', function(req, res) {
   res.render('public/pages/contact-me');
+});
+
+app.get('/projects', function(req, res) {
+  res.render('public/pages/projects');
+});
+
+app.get('/projects/Dinersclub', function(req, res) {
+  res.render('public/pages/projects/dinersclub');
 });
 
 app.listen(port, () => {
